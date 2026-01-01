bcfb074d3281100021d696fd9163a30f11120e04
Alexander Lukashenko <fuck-lukashenko@proton.me>
2026-01-01 14:49:49 +0200

    Switch no native fetch

--- APIClient.js
+++ APIClient.js
@@ -1,31 +1,77 @@
-import axios from 'axios';
-
 class APIClient {
-  #instance;
+  #baseURL;
+  #defaultHeaders;
 
-  static get(...args) {
-    return new APIClient().get(...args);
+  static get(url, config) {
+    return new APIClient().get(url, config);
   }
 
-  static put(...args) {
-    return new APIClient().put(...args);
+  static put(url, data, config) {
+    return new APIClient().put(url, data, config);
   }
 
-  constructor(baseURL) {
-    this.#instance = axios.create({
-      baseURL,
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    this.#instance.interceptors.response.use(({ data }) => data);
+  constructor(baseURL = '') {
+    this.#baseURL = baseURL;
+    this.#defaultHeaders = { 'Content-Type': 'application/json' };
   }
 
-  get(...args) {
-    return this.#instance.get(...args);
+  async #request(endpoint, method, body = null, config = {}) {
+    let url = endpoint;
+    
+    if (this.#baseURL) {
+      const base = this.#baseURL.replace(/\/+$/, '');
+      const path = endpoint.replace(/^\/+/, '');
+      url = `${base}/${path}`;
+    }
+
+    if (config.params) {
+      const params = new URLSearchParams(config.params).toString();
+      url += `?${params}`;
+    }
+
+    const headers = {
+      ...this.#defaultHeaders,
+      ...(config.headers || {}),
+    };
+
+    const fetchConfig = {
+      method,
+      headers,
+    };
+
+    if (body) {
+      fetchConfig.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(url, fetchConfig);
+
+    const contentType = response.headers.get('content-type');
+    let data;
+    if (contentType && contentType.includes('application/json')) {
+      data = await response.json();
+    } else {
+      data = await response.text();
+    }
+
+    if (!response.ok) {
+      const error = new Error(`Request failed with status ${response.status}`);
+      error.response = {
+        status: response.status,
+        statusText: response.statusText,
+        data: data
+      };
+      throw error;
+    }
+
+    return data;
   }
 
-  put(...args) {
-    return this.#instance.put(...args);
+  get(url, config = {}) {
+    return this.#request(url, 'GET', null, config);
+  }
+
+  put(url, data, config = {}) {
+    return this.#request(url, 'PUT', data, config);
   }
 }
 
